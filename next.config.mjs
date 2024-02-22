ac21b734682172c73ebdbf29e5beb32a413b1cdd
Andrei Iobaj <Sebastianiobaj@yahoo.com>
2024-02-22 23:09:15 +0200

    fix 1.3

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
      */
+    output: "export",
 
     /**
      * Set base path. This is the slug of your GitHub repository.
