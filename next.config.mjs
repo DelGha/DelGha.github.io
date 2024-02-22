30c9fdfaf661165042431bee633175d1d5c2321f
Andrei Iobaj <Sebastianiobaj@yahoo.com>
2024-02-22 23:49:13 +0200

    fix 1.4

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,9 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/",
+    basePath: "/DelGha.github.io",
+
+    assetPrefix: isProd ? '/DelGha.github.io/' : '',
 
     /**
      * Disable server-based image optimization. Next.js does not support
