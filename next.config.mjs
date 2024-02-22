afa705199d10bc7b42982f2c2976d6a0bb2a2489
Andrei Iobaj <Sebastianiobaj@yahoo.com>
2024-02-22 23:01:12 +0200

    fix 1.2

--- next.config.mjs
+++ next.config.mjs
@@ -2,35 +2,14 @@
 import withVideos from "next-videos";
 
 const nextConfig = {
-    /**
-     * Enable static exports for the App Router.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
-    output: "export",
-
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/nextjs-github-pages",
-
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
     images: {
-        unoptimized: true,
-    },
+        loader: 'akamai',
+        path: '',
+      },
+      assetPrefix: './',
 
-    /**
-     * Custom webpack configuration
-     */
     webpack: (config, { isServer }) => {
-        // Add file-loader for mp4 files
+        
         config.module.rules.push({
             test: /\.(mp4)$/,
             use: [
@@ -38,7 +17,7 @@ const nextConfig = {
                     loader: 'file-loader',
                     options: {
                         publicPath: '/_next',
-                        outputPath: 'static/videos/', // Specify the output path for your videos
+                        outputPath: 'static/videos/', 
                         name: '[name].[hash].[ext]',
                     },
                 },
