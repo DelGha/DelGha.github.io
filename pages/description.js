34b49b2eee31487b796554802fb25edb1fb099f4
Andrei Iobaj <Sebastianiobaj@yahoo.com>
2024-03-03 18:14:14 +0200

    Adding written details and fixes

--- pages/description.js
+++ pages/description.js
@@ -1,6 +1,7 @@
 import styles from "../styles/Home.module.css";
 import { useEffect } from "react";
 import Link from "next/link";
+import Image from 'next/image'
 
 export default function Description() {
 
@@ -66,13 +67,25 @@ export default function Description() {
       <div className="flex flex-col items-center h-max justify-center w-screen relative z-10">
         {/* Top section */}
         <div className={`${styles['top']}`}>
-          <div className="bg-black rounded-lg p-8 flex center justify-center items-center mb-28 mt-20 border-solid">
+          <div className="bg-black rounded-lg p-8 flex center justify-center items-center mb-20 mt-20 border-solid">
             {/* Picture */}
-            <div className="w-48 h-48 rounded-full bg-white mr-8"></div>
+            <div className="w-56 h-32 rounded-full bg-white mr-8 flex justify-center items-center">
+              <Image src = {require("/public/my_picture.png")} alt = "Personal Picture" className="w-32 h-32 rounded-full p-1" />
+            </div>
             {/* Text */}
             <div>
               <h2 className="text-xl font-bold mb-2">Hello and welcome to my page!</h2>
-              <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut ligula vitae lorem vestibulum bibendum.</p>
+              <div className="text-gray-100">
+                <h1 className="mb-4 font-bold">
+                  Ambitious, Determined and Focused. This is my mentality. This is me! 
+                </h1>
+                <h1 className="mb-4">
+                  My name is Iobaj Andrei Sebastian, currently a student, in the 3rd year, English Computer Science Department, Faculty of Automatics and Computers, Technical University of Cluj-Napoca.
+                </h1>
+                <h1 className="mb-4">
+                  Persuing excelence at every given opportunity, striving for innovation in my search for knowledge and embracing challenges head-on are my desires for personal growth. Scroll below if you want to learn more about myself and what I do.
+                </h1>
+              </div>
             </div>
           </div>
         </div>
@@ -85,9 +98,36 @@ export default function Description() {
         </div>
 
         {/* Parallelograms */} 
-          <div className={`bg-black transform -skew-x-6 h-72 mb-48 mt-24 border-solid border-white border-x-2 border-y-2 ${styles['disappear-parallelogram-one']}`} style={{ width: '650px', marginRight: '650px' }}></div>
-          <div className={`bg-black transform skew-x-6 h-72 mb-48 border-solid border-white border-x-2 border-y-2 ${styles['disappear-parallelogram-two']}`} style={{ width: '650px', marginLeft: '650px' }}></div>
-          <div className={`bg-black transform -skew-x-6 h-72 mb-28 border-solid border-white border-x-2 border-y-2 ${styles['disappear-parallelogram-three']}`} style={{ width: '650px', marginRight: '650px' }}></div>
+          <div className={`bg-black transform -skew-x-6 h-72 mb-48 mt-24 border-solid border-white border-x-2 border-y-2 ${styles['disappear-parallelogram-one']}`} style={{ width: '650px', marginRight: '650px' }}>
+            <br></br>
+            <h1 className="transform skew-x-12 text-5xl mt-0 mb-4 ml-2 mr-2 animate-none"> &nbsp;Motivation: </h1>
+            <h2 className="transform skew-x-12 mt-1 mb-0 ml-2 mr-2 animate-none"> &nbsp;&nbsp; Studying computers, building hardware components and designing programs or systems, was the path I wanted to walk on in life, ever since, as a kid, I discovered the power this technology does hold. Knowledge above all else. </h2>
+            <br></br>
+            <h2 className="transform skew-x-12 mt-1 mb-0 ml-2 mr-2 animate-none">&nbsp;&nbsp; Now that I am presently learning the intricate details of how computers and large systems operate, I could not be happier being surrounded by what I enjoy most. Logic and Science!</h2>
+          </div>
+          <div className={`bg-black transform skew-x-6 h-72 mb-48 border-solid border-white border-x-2 border-y-2 ${styles['disappear-parallelogram-two']}`} style={{ width: '650px', marginLeft: '650px' }}>
+            <br></br>
+            <h1 className="transform text-5xl mt-0 mb-4 ml-2 mr-2 animate-none" style={{ transform: 'skewX(-26deg)' }}> &nbsp;Domains of Study & Passion: </h1>
+            <h2 className="transform skew-x-12 mt-1 mb-0 ml-4 mr-4 animate-none" style={{ transform: 'skewX(-26deg)' }}> &nbsp;&nbsp; Throughout the years, I was able to come into contact with a lot new technologies and concepts. The ones that I found to be the most interesting are: 
+              <ul className="ml-16" style={{ listStyleType: 'disc' }}>
+                <li>The subject of AI and problem modelling</li>
+                <li>Hardware Systems design and implementation</li>
+                <li>Software programs development for complex problem solving</li>
+              </ul>
+            </h2>
+            <h2 className="transform skew-x-12 mt-1 mb-0 ml-4 mr-4 animate-none" style={{ transform: 'skewX(-26deg)' }}> &nbsp;&nbsp; I am now confident that, in my future carrer, at least one of the above topics is my domain of choice and I can firmly state that I will give my all to achieve my dreams!</h2>
+          </div>
+          <div className={`bg-black transform -skew-x-6 h-72 mb-28 border-solid border-white border-x-2 border-y-2 ${styles['disappear-parallelogram-three']}`} style={{ width: '650px', marginRight: '650px' }}>
+            <br></br>
+            <h1 className="transform skew-x-12 text-5xl mt-0 mb-4 ml-2 mr-2 animate-none"> &nbsp;Projects: </h1>
+            <h2 className="transform skew-x-12 mt-1 mb-0 ml-2 mr-2 animate-none"> &nbsp;&nbsp; Most of my projects can be found on my <Link href={'https://github.com/DelGha'} className="font-bold">Github</Link>. Some of my most interesting and challenging projects so far are: </h2>
+            <ul className="ml-16" style={{ listStyleType: 'disc' }}>
+                <li>Robotic Arm using Arduino</li>
+                <li>Among Us Simulator using FOL</li>
+                <li>This presentation and the E-Commerce sites using React</li>
+              </ul>
+              <h2 className="transform skew-x-12 mt-1 mb-0 ml-2 mr-2 animate-none"> &nbsp;&nbsp; The desire of achieving something staggering through my work and project has never been stronger. I want to create something that both me and others are proud of. I want my work to speak for me! To be me!</h2>
+          </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -17,13 +17,13 @@ export default function HomePage() {
       <div className="absolute inset-0 flex justify-center items-center">
         <div className={styles.buttons}>
           <div className="flex flex-col items-center">
-            <Link href={'https://github.com/DelGha?tab=repositories'} className="bg-transparent" >
+            <Link href={'https://github.com/DelGha'} className="bg-transparent" >
               <button className="bg-white p-2 px-4 rounded-lg mb-4">
                 My Github
               </button>
             </Link>
 
-            <Link href={'/'} className="bg-transparent" >
+            <Link href={'https://www.linkedin.com/in/andrei-iobaj-8227702b7/'} className="bg-transparent" >
               <button className="bg-white p-2 px-4 rounded-lg mb-4">
                 linkedIn
               </button>
@@ -31,7 +31,7 @@ export default function HomePage() {
 
             <Link href={'/'} className="bg-transparent" >
               <button className="bg-white p-2 px-4 rounded-lg mb-4">
-                VEDEM LATER
+                To be added
               </button>
             </Link>
 
@@ -51,7 +51,7 @@ export default function HomePage() {
             
             <Link href={'/'} className="bg-transparent" >
               <button className="bg-white p-2 px-4 rounded-lg mb-4">
-                VEDEM LATER
+                To be added
               </button>
             </Link>
           </div>
